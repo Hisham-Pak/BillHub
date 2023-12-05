0f4d58412c5ef0f8b1b40c2976be71d7a0ad5d86
Hisham Mahmood <hishammahmood41@gmail.com>
2023-12-05 22:53:19 +0500

    Add create customer gui

--- source/main.c
+++ source/main.c
@@ -1,3 +1,5 @@
+#include <float.h>
+#include <limits.h>
 #include <GL/glew.h>
 
 #define MAX_VERTEX_BUFFER 512 * 1024
@@ -19,12 +21,78 @@
 static void error_callback(int e, const char *d)
 {printf("Error %d: %s\n", e, d);}
 
+/* ===============================================================
+ *
+ *                          CUSTOMER
+ *
+ * ===============================================================*/
+struct date{
+    int month;
+    int day;
+    int year;
+};
+
+struct account {
+	int acct_no;
+	char name[100];
+	char mobile_no[30];
+	char street[100];
+	char city[100];
+	float oldbalance;
+	float newbalance;
+	float payment;
+	struct date paymentdate;
+} customer;
+
+static void customer_window(struct nk_context *ctx)
+{
+	if (nk_begin(ctx, "Create Customer", nk_rect(50, 50, 400, 400), NK_WINDOW_BORDER|NK_WINDOW_MOVABLE|NK_WINDOW_CLOSABLE)) {
+		nk_layout_row_dynamic(ctx, 30, 2);
+		
+		nk_label(ctx, "Account No:", NK_TEXT_LEFT);
+		nk_property_int(ctx, "#Account No:", INT_MIN, &customer.acct_no, INT_MAX, 1, 1);
+		
+        nk_label(ctx, "Name:", NK_TEXT_LEFT);
+		nk_edit_string_zero_terminated(ctx, NK_EDIT_FIELD, customer.name, sizeof(customer.name), nk_filter_default);
+		
+		nk_label(ctx, "Mobile No:", NK_TEXT_LEFT);
+		nk_edit_string_zero_terminated(ctx, NK_EDIT_FIELD, customer.mobile_no, sizeof(customer.mobile_no), nk_filter_default);
+		
+		nk_label(ctx, "Street:", NK_TEXT_LEFT);
+		nk_edit_string_zero_terminated(ctx, NK_EDIT_FIELD, customer.street, sizeof(customer.street), nk_filter_default);
+		
+		nk_label(ctx, "City:", NK_TEXT_LEFT);
+		nk_edit_string_zero_terminated(ctx, NK_EDIT_FIELD, customer.city, sizeof(customer.city), nk_filter_default);
+
+        nk_label(ctx, "Old Balance:", NK_TEXT_LEFT);
+        nk_property_float(ctx, "#Old Balance:", FLT_MIN, &customer.oldbalance, FLT_MAX, 1, 1);
+
+        nk_label(ctx, "New Balance:", NK_TEXT_LEFT);
+        nk_property_float(ctx, "#New Balance:", FLT_MIN, &customer.newbalance, FLT_MAX, 1, 1);
+
+        nk_label(ctx, "Payment:", NK_TEXT_LEFT);
+        nk_property_float(ctx, "#Payment:", FLT_MIN, &customer.payment, FLT_MAX, 1, 1);
+
+		// Add more fields as needed...
+		
+		if (nk_button_label(ctx, "Submit")) {
+			// Handle form submission...
+			printf("Account No: %d\n", customer.acct_no);
+			printf("Name: %s\n", customer.name);
+			printf("Mobile No: %s\n", customer.mobile_no);
+			printf("Street: %s\n", customer.street);
+			printf("City: %s\n", customer.city);
+            printf("Old Balance: %.2f\n", customer.oldbalance);
+            printf("New Balance: %.2f\n", customer.newbalance);
+            printf("Payment: %.2f\n", customer.payment);
+			// Print more fields as needed...
+		}
+	}
+	nk_end(ctx);
+}
+
 int main(void)
 {
-    /* Platform */
-    int win_width = 200;
-    int win_height = 250;
-
     /* GLFW */
     struct nk_glfw glfw = {0};
     static GLFWwindow *win;
@@ -65,19 +133,7 @@ int main(void)
         nk_glfw3_new_frame(&glfw);
 
         /* GUI */
-        if (nk_begin(ctx, "Anything window", nk_rect(0, 0, win_width, win_height), 0))
-        {
-            nk_layout_row_dynamic(ctx, 120, 1);
-            nk_label(ctx, "Hello world!", NK_TEXT_CENTERED);
-            
-            nk_layout_row_dynamic(ctx, 50, 1);
-            nk_label(ctx, "Hello world!", NK_TEXT_LEFT);
-            
-            nk_layout_row_static(ctx, 30, 80, 1);
-            if (nk_button_label(ctx, "AnyButton"))
-                fprintf(stdout, "button pressed\n");
-        }
-        nk_end(ctx);
+        customer_window(ctx);
 
         /* Draw */
         glViewport(0, 0, win_width, win_height);
